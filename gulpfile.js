c5ed6ca3e994818320ddce053f44a3de87dd9858
Pepe Nunez <josenunezdiaz@gmail.com>
2016-10-08 10:04:23 +1100

    Updates Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -21,8 +21,15 @@ var lib_dir = 'bower_components/';
 var build_dir = "dist/app/";
 var dist_dir = "dist/";
 
-gulp.task('scripts', function() {
 
+var tasks = {
+	once: [],
+	watch: [],
+};
+
+/* JAVASCRIPT ____________________________________________________________________________*/
+var scripts_src = 'src/js/**';
+gulp.task('scripts', function() {
 	var scripts = [
 		// lib_dir + 'angular/angular.min.js',
 		
@@ -44,6 +51,7 @@ gulp.task('scripts', function() {
 
 		/*src_dir + 'js/data_models/common.js',
 		src_dir + 'js/data_models/product.js',*/
+		
 		src_dir + 'js/data_models/*',
 		
 		src_dir + 'js/app.js',
@@ -70,16 +78,28 @@ gulp.task('scripts', function() {
 		.pipe(uglify())
 			.pipe(gulp.dest(build_dir+'js'));
 });
+gulp.task('scripts_w', function(){gulp.watch(scripts_src,['scripts']);});
+tasks.once.push('scripts');
+tasks.watch.push('scripts_w');
 
+
+/* IMAGES ____________________________________________________________________________*/
+var image_min_src = 'src/img/**';
 gulp.task('image_min', function(){
-	gulp.src('src/img/**')
+	gulp.src(image_min_src)
 		.pipe(imagemin())
 		.pipe(gulp.dest(build_dir+'img/'))
 	;
 });
+gulp.task('image_min_w', function(){gulp.watch(image_min_src,['image_min']);});
+tasks.once.push('image_min');
+tasks.watch.push('image_min_w');
 
+
+/* SCSS ____________________________________________________________________________*/
+var sass_compile_src = 'src/css/*.scss';
 gulp.task('sass_compile',function(){
-	gulp.src('src/css/*.scss')
+	gulp.src(sass_compile_src)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 3 versions','safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
@@ -89,35 +109,50 @@ gulp.task('sass_compile',function(){
 		.pipe(gulp.dest(build_dir+'css'))
 	;
 });
+gulp.task('sass_compile_w',function(){gulp.watch(sass_compile_src,['sass_compile']);});
+tasks.once.push('sass_compile');
+tasks.watch.push('sass_compile_w');
 
+
+/* HTML ____________________________________________________________________________*/
+var html_min_src = 'src/**/*.html';
 gulp.task('html_min',function(){
-	gulp.src('src/**/*.html')
+	gulp.src(html_min_src)
 		.pipe(htmlmin({collapseWhitespace: true}))
 		.pipe(gulp.dest(build_dir))
 	;
 });
+gulp.task('html_min_w',function(){gulp.watch(html_min_src,['html_min']);});
+tasks.once.push('html_min');
+tasks.watch.push('html_min_w');
 
+
+/* FONTS ____________________________________________________________________________*/
+var copyfonts_src = 'src/fonts/**';
 gulp.task('copyfonts', function() {
-   gulp.src('src/fonts/**')
+   gulp.src(copyfonts_src)
    .pipe(gulp.dest(build_dir+'fonts'));
 });
+gulp.task('copyfonts_w', function() { gulp.watch(copyfonts_src,['copyfonts']);});
+tasks.once.push('copyfonts');
+tasks.watch.push('copyfonts_w');
 
+
+/* packagejson ____________________________________________________________________________*/
+var packagejson_src = 'package.json';
 gulp.task('packagejson', function() {
-   gulp.src('package.json')
+   gulp.src(packagejson_src)
    .pipe(gulp.dest(dist_dir));
 });
+gulp.task('packagejson_w', function(){gulp.watch(packagejson_src,['packagejson']);});
+tasks.once.push('packagejson');
+tasks.watch.push('packagejson_w');
 
-gulp.task('compile_once',['scripts','copyfonts','sass_compile','html_min','image_min','packagejson']);
-gulp.task('compile_watch',function() {
-	gulp.watch('src/css/*.scss',['sass_compile']);
-	gulp.watch('src/**/*.html',['html_min']);
-	gulp.watch('src/img/*',['image_min']);
-	gulp.watch('src/fonts/**',['copyfonts']);
-	
-	gulp.watch('src/js/**/*.js',['scripts']);
-	gulp.watch('package.json',['packagejson']);
-});
-gulp.task('default',['compile_once','compile_watch']);
+
+/* GENERAL ____________________________________________________________________________*/
+gulp.task('once',tasks.once);
+gulp.task('watch',tasks.watch);
+gulp.task('default',['once','watch']);
 
 /*
 	VERSIONING & RELASES ______________________________________________________________________
